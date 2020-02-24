467836f8492330c032a661f903f71ca8ae4abec5
Juan M. Panasiti <juanpanasiti@gmail.com>
2020-02-24 18:27:15 -0300

    se agregaron validaciones al modelo Device

--- app/models/device.rb
+++ app/models/device.rb
@@ -1,4 +1,32 @@
 class Device < ApplicationRecord
+  ####### Relation
   belongs_to :deviceModel
   belongs_to :line
+
+  ####### Validations
+  validates :imei, presence: true
+  validates :imei, uniqueness: true
+  validates :imei, format: { with: /\A[0-9]{15}+\z/, message: "must be only 15 numeric characters" }
+  #validates :imei, length: { is: 15 }
+  validates :device_model_id, presence: true
+  validates :line_id, presence: true
+  validates :status, presence: true
+  validates :isExternal, inclusion: { in: [true, false] }
+  validates :isExternal, exclusion: { in: [nil] }
+  #"details" no tiene validaciones aún
+  validate :expiration_date_cannot_be_in_the_past,
+    :discount_cannot_be_greater_than_total_value
+
+#  def expiration_date_cannot_be_in_the_past
+#    if expiration_date.present? && expiration_date < Date.today
+#      errors.add(:expiration_date, "can't be in the past")
+#    end
+#  end
+#
+#  def discount_cannot_be_greater_than_total_value
+#    if discount > total_value
+#      errors.add(:discount, "can't be greater than total value")
+#    end
+#  end
+  #######
 end
