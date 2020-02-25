51ae04017af370a76202ac54ff266b7928950974
Juan M. Panasiti <juanpanasiti@gmail.com>
2020-02-25 00:20:18 -0300

    Embellecimiento de las vistas de login y signup

--- app/models/admin.rb
+++ app/models/admin.rb
@@ -3,4 +3,14 @@ class Admin < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  #######
+  def set_level(level)
+    this.access_level = level
+    this.save
+  end
+
+  def get_email
+    return self.email.split('@').first
+  end
 end
