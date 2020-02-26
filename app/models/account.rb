ca6993634957b543aee8910fd971f2725609d2d0
Juan Panasiti <juanpanasiti@gmail.com>
2020-02-26 13:41:20 -0300

    Creado controlador de Accounts y vistas de new.js y _form

--- app/models/account.rb
+++ app/models/account.rb
@@ -1,2 +1,11 @@
 class Account < ApplicationRecord
+  #t.string "name"
+  #t.string "number"
+  ############ RELATIONS
+  has_many :lines
+  ############ VALIDATIONS
+  validates :name, presence: true
+  validates :number, presence: true
+  validates :number, uniqueness: true
+  ############
 end
