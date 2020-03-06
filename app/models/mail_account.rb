593bf01b0992ae89c57ec41f50fd94954f4eda74
Juan M. Panasiti <juanpanasiti@gmail.com>
2020-03-05 22:33:25 -0300

    Agregadas validaciones al modelo de MailAccount

--- app/models/mail_account.rb
+++ app/models/mail_account.rb
@@ -1,3 +1,21 @@
 class MailAccount < ApplicationRecord
+  #t.bigint "user_id", null: false
+  #t.string "email"
+  #t.string "pass"
+  #t.date "date"
+  #t.string "note"
+  ############ RELATIONS
   belongs_to :user
+
+  ############ VALIDATIONS
+  validates :user_id, presence:true
+  validates :email, presence:true
+  validates :date, presence:true
+  ############ CALLBACKS
+
+  ############ METHODS
+
+  ############ CLASS METHODS
+
+  #############CALLBACKS METHODS
 end
