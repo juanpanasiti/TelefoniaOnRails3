64250831a5889848e77d4cd6135dee314b8da9c8
Juan Marcelo Panasiti <juanpanasiti@gmail.com>
2020-04-01 17:22:19 -0300

    Metodos para mostrar el total de lineas por oficina y diferenciar las lineas directas de las lines de las suboficinas

--- app/models/office.rb
+++ app/models/office.rb
@@ -5,6 +5,8 @@ class Office < ApplicationRecord
   ############ RELATIONS
   belongs_to :office, optional: true
   has_many :offices
+  has_many :users
+  has_many :lines, through: :users
   ############ VALIDATIONS
   validates :name, presence:true
   validates :category, presence:true
@@ -30,6 +32,25 @@ class Office < ApplicationRecord
       return ""
     end
   end#get_father_fullname
+
+  def count_lines
+    count = self.lines.count
+    self.offices.each do |suboffice|
+      count += suboffice.count_lines
+    end
+
+    return count
+  end#count_lines
+
+  def get_count_lines
+    office_lines = self.lines.count
+    suboffices_lines = 0
+    self.offices.each do |suboffice|
+      suboffices_lines += suboffice.count_lines
+    end
+    total_lines = office_lines + suboffices_lines
+    return "#{total_lines} (#{office_lines} + #{suboffices_lines})"
+  end#get_count_lines
   ############ CLASS METHODS
   def self.get_category_options
     return FormOption.get_options_for("office_category")
