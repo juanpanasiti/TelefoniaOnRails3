c61c2029732134f4685fa1119eb86cf7b3f50b60
Juan Marcelo Panasiti <juanpanasiti@gmail.com>
2020-04-01 09:11:04 -0300

    Mejora en la vista que muestra el contenido de los reportes

--- app/helpers/csv_report_lines_helper.rb
+++ app/helpers/csv_report_lines_helper.rb
@@ -1,2 +1,17 @@
 module CsvReportLinesHelper
-end
+  def truncate_last(number,positions)
+    unless number.length == 0
+      return "~#{number.to_s.last(positions)}"
+    else
+      return ""
+    end
+  end#truncate_last()
+
+  def format_device_name(model)
+    if model.upcase == "EQUIPO DESCONOCIDO"
+      return "DESCONOCIDO"
+    else
+      return model.sub(" T","").sub(" LTE","").sub(" NB","")
+    end
+  end
+end#module
